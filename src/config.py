f0c9bd1846a62c4e757affa9f47d6355cf114f79
Chatsito GPT <chatsitogpt58@gmail.com>
2025-07-02 16:21:23 -0500

    Add secondary DB env vars

--- src/config.py
+++ src/config.py
@@ -10,6 +10,11 @@ class Config:
     DB_REMOTE_USER = os.getenv('DB_REMOTE_USER', 'root')
     DB_REMOTE_PASSWORD = os.getenv('DB_REMOTE_PASSWORD', '')
     DB_REMOTE_NAME = os.getenv('DB_REMOTE_NAME', 'alquiler_vehiculos')
+    DB_REMOTE_HOST2 = os.getenv('DB_REMOTE_HOST2', 'localhost')
+    DB_REMOTE_PORT2 = os.getenv('DB_REMOTE_PORT2', '3306')
+    DB_REMOTE_USER2 = os.getenv('DB_REMOTE_USER2', 'root')
+    DB_REMOTE_PASSWORD2 = os.getenv('DB_REMOTE_PASSWORD2', '')
+    DB_REMOTE_NAME2 = os.getenv('DB_REMOTE_NAME2', 'alquiler_vehiculos_2')
 
     # Configuración de seguridad
     MAX_LOGIN_ATTEMPTS = 3
